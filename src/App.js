54f5d126f018d01ca87c5902ce3f1d9447add460
fahime-npr98 <fahime.npr@gmail.com>
2021-11-12 16:36:38 +0330

    fir

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import 'antd/dist/antd.css';
+import { Button, Row, Col, Form, Input } from 'antd';
+
 
 function App() {
+    const [state, setstate] = useState([
+    {content:"todo1", id:1},
+    {content:"todo2", id:2},
+    {content:"todo3", id:3},
+    ])
+    const test=state.map((states)=>console.log(state.content));
+    const onFinish= e =>{ console.log(e);}
+      
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+   <Form name="content" >
+     <Row>
+       <Col>
+        <Form.Item name="content" onFinish={onFinish} >
+          <Input type="text" />
+        </Form.Item> 
+         <Button htmlType="submit">
+            CREAT
+          </Button>
+       </Col>
+     </Row>
+    </Form>
+    <Row>
+       <Col>
+         
+       </Col>
+     </Row>
+    
+
+  </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
+import {ConfigProvider} from "antd"
 
 ReactDOM.render(
   <React.StrictMode>
+    <ConfigProvider>
     <App />
+    </ConfigProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
